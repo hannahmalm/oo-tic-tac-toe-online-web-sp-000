c15c61dc49ea8fd4a461c8ab82c9bd709c6fa009
Hannah Malm <hannahemalm616@gmail.com>
2021-02-09 23:54:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end 
   
-  def valid_move?(board,index)
+  def valid_move?(index)
     if index.between?(0,8) && position_taken?(@board,index) == false
         true
     else 
