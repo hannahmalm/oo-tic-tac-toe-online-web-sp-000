0d87b0c7ea03e141e247a72e5ee18b3c69b590d1
Hannah Malm <hannahemalm616@gmail.com>
2021-02-09 23:57:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ class TicTacToe
     @board[index] = current_player
   end 
   
-  def position_taken?(board,index)
+  def position_taken?(index)
     !(@board[index].nil? || @board[index] == " ")
   end 
   
