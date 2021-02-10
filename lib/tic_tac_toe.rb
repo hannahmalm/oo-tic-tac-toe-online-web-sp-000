67d03b9e167dd45504a8ebf73c6b3d0abd4f3a05
Hannah Malm <hannahemalm616@gmail.com>
2021-02-10 01:04:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,7 +72,7 @@ class TicTacToe
     WIN_COMBINATIONS.detect do |i|
     @board[i[0]] == @board[i[1]] &&
     @board[i[1]] == @board[i[2]] &&
-    position_taken?(index) 
+    position_taken?(@board) 
     end 
   end 
   
